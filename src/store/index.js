72bca0796d398a1dbb217c346997a166e86ab40b
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-22 11:40:46 +0800

    Connect redux toolkit state

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,7 @@
-import { createStore } from 'redux';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 const initialState = { counter: 0, showCounter: true };
 
-createSlice({
+const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
@@ -21,39 +20,41 @@ createSlice({
   }
 });
 
-const counterReducer = (state = initialState, action) => {
-  if (action.type === 'increment') {
-    return { 
-      counter: state.counter + 1, 
-      showCounter: state.showCounter,
-    };
-  }
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === 'increment') {
+//     return { 
+//       counter: state.counter + 1, 
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === 'increase') {
-    return { 
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
+//   if (action.type === 'increase') {
+//     return { 
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === 'decrement') {
-    return { 
-      counter: state.counter - 1,
-      showCounter: state.showCounter,
-    };
-  }
+//   if (action.type === 'decrement') {
+//     return { 
+//       counter: state.counter - 1,
+//       showCounter: state.showCounter,
+//     };
+//   }
 
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    }
-  }
+//   if (action.type === 'toggle') {
+//     return {
+//       showCounter: !state.showCounter,
+//       counter: state.counter,
+//     }
+//   }
 
-  return state;
+//   return state;
 
-};
+// };
 
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: counterSlice.reducer
+});
 
 export default store;
